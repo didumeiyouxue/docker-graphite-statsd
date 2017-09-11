035e565138a106b455a322adff4c4735b6e57604
Zhang Hui <zhanghui@wespoke.com>
2017-09-11 17:26:45 +0800

    fix config

--- conf/opt/statsd/config.js
+++ conf/opt/statsd/config.js
@@ -2,9 +2,11 @@
   "graphiteHost": "127.0.0.1",
   "graphitePort": 2003,
   "port": 8125,
-  "flushInterval": 10000
-  backends: [ "./backends/graphite", "statsd-zabbix-backend" ],
-  zabbixPort: 10051,
-  zabbixHost: "10.10.0.52",
-  zabbixSender: "/usr/bin/zabbix_sender"
+  "flushInterval": 10000,
+  "debug": true,
+  "percentThreshold": [95, 99],
+  "backends": ["statsd-zabbix-backend"],
+  "zabbixPort": 10051,
+  "zabbixHost": "localhost",
+  "zabbixSender": "/usr/bin/zabbix_sender"
 }
