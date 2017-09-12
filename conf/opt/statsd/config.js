8ae5a3324e68532243e38bd075009358ca004fb6
Zhang Hui <zhanghui@wespoke.com>
2017-09-12 12:14:16 +0800

    zabbix ip

--- conf/opt/statsd/config.js
+++ conf/opt/statsd/config.js
@@ -7,6 +7,6 @@
   "percentThreshold": [95, 99],
   "backends": ["./backends/graphite", "./node_modules/statsd-zabbix-backend"],
   "zabbixPort": 10051,
-  "zabbixHost": "localhost",
+  "zabbixHost": "10.10.0.52",
   "zabbixSender": "/usr/bin/zabbix_sender"
 }
